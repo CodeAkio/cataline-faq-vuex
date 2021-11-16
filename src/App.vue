c4d1d53afa2ac59ae28099fb0c74de047df0bd94
CodeAkio <siqueira_2004@hotmail.com>
2021-11-16 20:30:22 -0300

    Create card component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <h1>Hello</h1>
+  <div class="container">
+    <Card />
+  </div>
 </template>
 
 <script>
+import Card from '@/components/Card'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Card
+  }
 }
 </script>
 
 <style>
+.container {
+  height: 100vh;
+  background-color: #f5f6f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style scoped>
+.card {
+  width: 305px;
+  height: 457px;
+  background: transparent linear-gradient(180deg, #404451 0%, #26282c 100%) 0%
+    0% no-repeat padding-box;
+  box-shadow: 0px 10px 30px #3f445266;
+  border-radius: 15px;
+}
+</style>
