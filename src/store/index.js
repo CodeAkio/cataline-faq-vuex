1ec5f68fccfa6bafd5044589e726676c6dc48fb5
CodeAkio <siqueira_2004@hotmail.com>
2021-11-17 13:59:36 -0300

    Add store

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,54 @@
-import { createStore } from "vuex";
+import { createStore } from 'vuex'
+import { faqCategories } from '../../db.json'
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
-});
+  state: {
+    categories: [],
+    view: '',
+    id: null,
+    answerId: null
+  },
+  actions: {
+    fetchCategories({ commit }) {
+      const categories = faqCategories
+      commit('SET_CATEGORIES', categories)
+    },
+    componentView({ commit }, view) {
+      commit('changeView', view)
+    },
+    questionId({ commit }, id) {
+      commit('getId', id)
+    },
+    answerId({ commit }, id) {
+      commit('getAnswerId', id)
+    }
+  },
+  mutations: {
+    SET_CATEGORIES(state, categories) {
+      state.categories = categories
+    },
+    changeView(state, view) {
+      state.view = view
+    },
+    myQuestionId(state, id) {
+      state.id = id
+    },
+    myAnswerId(state, id) {
+      state.answerId = id
+    }
+  },
+  getters: {
+    $allCategories(state) {
+      return state.categories
+    },
+    $returnView(state) {
+      return state.view
+    },
+    $returnId(state) {
+      return state.id
+    },
+    $returnAnswerId(state) {
+      return state.answerId
+    }
+  }
+})
